e8f11c6cdea9c014b723f3b3dc95151e4d664ff8
DESKTOP-HB3B4MH\Eileen <yiqianc.14@gmail.com>
2023-08-15 17:17:55 +0800

    feature: 加入 bootstrap、客製化 bootstrap 樣式

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+const { VITE_APP_URL, VITE_APP_PATH } = import.meta.env
+
+export default {
+  data() {
+    return {
+      isLoading: false,
+    }
+  },
+  components: {
+    RouterView,
+    RouterLink,
+  },
+  mounted() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000)
+
+    console.log(import.meta.env.VITE_APP_URL, import.meta.env.VITE_APP_PATH);
+    console.log(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products/all`);
+
+    this.$http.get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products/all`)
+      // axios.get('https://randomuser.me/api/')
+      .then((res) => {
+        console.log(res);
+      })
+
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <header>
+      <VueLoading v-model:active="isLoading"></VueLoading>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink to="/">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink to="/about">About</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink to="/bill">Bill</RouterLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <RouterView />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import './assets/all.scss';
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueAxios, axios) // 套件
+
+app.component('VueLoading', Loading) //全域元件
+app.mount('#app')
